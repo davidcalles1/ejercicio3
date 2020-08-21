98161f522fa1daefa482d6d829afe600ae94c1ca
nelson calles <cc0324032019@unab.edu.sv>
2020-08-20 19:35:08 -0600

    primer avance

---
+++ Clase02Ejercicio03/src/main/java/com/unab/clase02/clase02ejercicio03/Persona.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.clase02.clase02ejercicio03;
+
+/**
+ *
+ * @author nelson
+ */
+public class Persona {
+    
+}
